a9425342e63e355d4c1fcaa378c1a84aa5296870
Ubuntu <mansanher@gmail.com>
2023-04-16 22:26:32 +0000

    adding progress bar

--- transcript-fastapi/gunicorn_conf.py
+++ transcript-fastapi/gunicorn_conf.py
@@ -2,6 +2,9 @@
 # From https://www.vultr.com/docs/how-to-deploy-fastapi-applications-with-gunicorn-and-nginx-on-ubuntu-20-04/
 
 from multiprocessing import cpu_count
+import os
+
+API_TIMEOUT=float(os.environ["API_TIMEOUT"])
 
 # Socket Path
 bind = ['0.0.0.0:8000']
@@ -9,7 +12,7 @@ bind = ['0.0.0.0:8000']
 # Worker Options
 workers = cpu_count() + 1
 worker_class = 'uvicorn.workers.UvicornWorker'
-timeout = 240 # in case we need to receive answer from bananatag
+timeout = API_TIMEOUT
 
 # Logging Options
 loglevel = 'debug'
